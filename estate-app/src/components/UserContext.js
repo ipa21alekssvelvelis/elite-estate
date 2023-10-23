6ebe4a465d8ddedee42ab543d7a23179455a11c0
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-10-23 18:57:08 +0300

    Added UserContext, Logout components

--- estate-app/src/components/UserContext.js
+++ estate-app/src/components/UserContext.js
@@ -1,2 +1,39 @@
+
 import React, { createContext, useContext, useState } from 'react';
 import Cookies from 'js-cookie';
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+    const [userInfo, setUserInfo] = useState(() => {
+    const cookieUserID = Cookies.get('roleID');
+
+    if (cookieUserID) {
+      return { roleID: parseInt(cookieUserID) };
+    } else {
+      return { roleID: null };
+    }
+  });
+
+  const Login = (roleID) => {
+    // console.log("Setting roleID and cookie:", userID);
+    Cookies.set('roleID', roleID.toString());
+    setUserInfo({ roleID });
+    // console.log("Local storage and cookie operation successful:", userID);
+  };
+
+  const Logout = () => {
+    setUserInfo(null);
+    Cookies.remove('roleID');
+  };
+
+  return (
+    <UserContext.Provider value={{ userInfo, Login, Logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
