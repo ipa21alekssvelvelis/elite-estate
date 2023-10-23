5fb8ef261223d82b57c4719c6d5e3127675f406f
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-10-23 19:03:47 +0300

    Updated components folder

---
+++ estate-app/src/components/Logout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import { useUser } from './UserContext';
+
+function Logout() {
+  const { Logout } = useUser();
+
+  const handleLogout = () => {
+    Logout();
+    window.location.href = 'http://localhost:3000';
+  };
+
+  return (
+    <a onClick={handleLogout}>Logout</a>
+  );
+}
+
+export default Logout;
---
+++ estate-app/src/components/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../App.css';
+import Logout from './Logout.js';
+import Cookies from 'js-cookie';
+
+function Header() {
+  const isAuthenticated = Cookies.get('roleID');
+  const currentPath = window.location.pathname;
+
+  return (
+    <div className='header'>
+      <Logout />
+      {isAuthenticated && (
+        <>
+          {currentPath === '/' && (
+            <>
+              <a href={`/create`}>Create</a>
+              <a href={`/find`}>Find</a>
+            </>
+          )}
+          {currentPath === '/find' && (
+            <>
+              <a href={`/create`}>Create</a>
+              <a href={`/`}>List</a>
+            </>
+          )}
+          {currentPath === '/create' && (
+            <>
+              <a href={`/find`}>Find</a>
+              <a href={`/`}>List</a>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Header;
