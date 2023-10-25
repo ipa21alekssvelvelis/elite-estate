3a94ea386367d8c1518490788f4e9a9b98bcac7e
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-10-25 08:55:18 +0300

    Updated Login and Register components

--- estate-app/src/components/Login.js
+++ estate-app/src/components/Login.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 function Login() {
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+    const [errors, setErrors] = useState({});
 
+    const handleUsernameChange = (e) => {
+        setUser(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPass(e.target.value);
+    };
 }
 
 export default Login;
--- estate-app/src/components/Register.js
+++ estate-app/src/components/Register.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 function Register(){
+    const [user, setUser] = useState('');
+    const [pass, setPass] = useState('');
+    const [confirm, setConfirm] = useState('');
+    const [errors, setErrors] = useState({});
 
+    handleUsernameChange = (e) => {
+        setUser(e.target.value);
+    }
+
+    handlePasswordcHANGE = (e) => {
+        setPass(e.target.value);
+    }
+
+    handleConfirmChange = (e) => {
+        setConfirm(e.target.value);
+    }
 }
 
 export default Register;
