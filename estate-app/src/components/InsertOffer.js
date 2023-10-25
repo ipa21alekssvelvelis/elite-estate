bea05aa9e33d09562d3bf7eeba677f89363c60bd
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-10-25 09:06:25 +0300

    Added InsertOffer component

---
+++ estate-app/src/components/InsertOffer.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import '../styles/App.css';
+
+function InsertOffer(){
+    
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [RoomCount, setRoomCount] = useState('');
+    const [BathroomCount, setBathroomCount] = useState('');
+    const [BedroomCount, setBedroomCount] = useState('');
+    const [size, setSize] = useState('');
+    const [AreaSize, setAreaSize] = useState('');
+    const [price, setPrice] = useState('');
+    const [HouseType, setHouseType] = useState('');
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value);
+    };
+
+    const handleRoomCountChange = (e) => {
+        setRoomCount(e.target.value);
+    };
+
+    const handleBathroomCountChange = (e) => {
+        setBathroomCount(e.target.value);
+    };
+
+    const handleBedroomCountChange = (e) => {
+        setBedroomCount(e.target.value);
+    };
+
+    const handleSizeChange = (e) => {
+        setSize(e.target.value);
+    };
+
+    const handleAreaSizeChange = (e) => {
+        setAreaSize(e.target.value);
+    };
+
+    const handlePriceChange = (e) => {
+        setPrice(e.target.value);
+    };
+
+    const handleHouseTypeChange = (e) => {
+        setHouseType(e.target.value);
+    };
+}
+
+export default InsertOffer;
